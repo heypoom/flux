eaf82dc71954c2e48f3c550d3406cb6340153f78
Phoomparin Mano <phoomparin@gmail.com>
2019-04-15 19:09:01 +0700

    :tada: Initialized Repository

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class Main {
+    private final static String[] options = new String[] {"--present", "Sketch"};
+
+    public static void main(String[] args) {
+        PApplet.main(options);
+    }
+}
